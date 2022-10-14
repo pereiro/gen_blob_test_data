aff8c1b3af944f65818ef9cb5a74f0539d7b4b58
Sergey Bobretsov <s.bobretsov@gmail.com>
2022-10-14 12:56:21 +0300

    multithreads

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,75 @@
+use std::fmt::format;
 use std::fs::File;
+use std::path::Path;
+use std::thread;
 use rand::Rng;
 use serde::{Serialize};
 use flate2::Compression;
 use flate2::write::GzEncoder;
 use tar::Header;
 use clap::Parser;
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::time::Duration;
+use uuid::Uuid;
 
 #[derive(Parser,Debug)]
 struct Args {
-    ///Path to archive with testdata to create
+    ///Paths to archive with testdata to create
     #[arg(short, long)]
-    path: String,
+    paths: Vec<String>,
     ///Count of testdata files in the archive
-    #[arg(short, long, default_value_t = 1000)]
+    #[arg(short, long)]
     count: u64,
+    ///Threads per path
+    #[arg(short, long)]
+    threads_per_path: usize,
+    ///Files per thread
+    #[arg(short, long)]
+    files_per_thread: usize
 }
+
+static GLOBAL_THREAD_COUNT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 fn main() -> std::io::Result<()>{
     let args:Args = Args::parse();
 
-    let tar_gz = File::create(args.path)?;
-    let enc = GzEncoder::new(tar_gz,Compression::best());
-    let mut archive = tar::Builder::new(enc);
-    for _ in 0..args.count {
-        let testdata = RandomTestData::new();
-        let json = serde_json::to_string(&testdata)?;
-        //println!("{}",json);
-        let mut header = Header::new_gnu();
-        header.set_path(testdata.get_string_uid())?;
-        header.set_size(json.as_bytes().len() as u64);
-        header.set_cksum();
-        archive.append(&header,json.as_bytes())?;
+    for path in args.paths.clone(){
+        for i in 0..args.threads_per_path{
+            let path = path.clone();
+            GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
+            thread::spawn(move ||{
+                for _ in 0..args.files_per_thread {
+                    let tar_gz = File::create(gen_filepath(path.clone())).unwrap();
+                    let enc = GzEncoder::new(tar_gz, Compression::best());
+                    let mut archive = tar::Builder::new(enc);
+                    for _ in 0..args.count {
+                        let testdata = RandomTestData::new();
+                        let json = serde_json::to_string(&testdata).unwrap();
+                        //println!("{}",json);
+                        let mut header = Header::new_gnu();
+                        header.set_path(testdata.get_string_uid()).unwrap();
+                        header.set_size(json.as_bytes().len() as u64);
+                        header.set_cksum();
+                        archive.append(&header, json.as_bytes()).unwrap();
+                    }
+                    archive.finish().unwrap();
+                }
+                GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
+            });
+
+        }
     }
-    archive.finish()?;
+    // Wait for other threads to finish.
+    while GLOBAL_THREAD_COUNT.load(Ordering::SeqCst) != 0 {
+        thread::sleep(Duration::from_millis(1));
+    }
+
     Ok(())
+}
+
+fn gen_filepath(path: String) -> String{
+    let path = Path::new(path.as_str());
+    path.join(Uuid::new_v4().to_string()).to_str().unwrap().to_string()
 }
 
 #[derive(Serialize)]
